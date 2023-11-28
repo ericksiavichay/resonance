428b46f214cd321c679e1c82a5f5469c7c9605fa
Erick Siavichay <ericksiavichay40@gmail.com>
2023-11-28 14:40:12 -0800

    added cuda capability

--- models/text.py
+++ models/text.py
@@ -3,8 +3,10 @@ from torch import nn
 
 
 class TextEncoder(nn.Module):
-    def __init__(self, freeze_base=True):
+    def __init__(self, freeze_base=True, device="cuda"):
         super().__init__()
+
+        self.device = device
 
         self.model_base = SentenceTransformer("BAAI/bge-large-en-v1.5")
         self.tokenizer_base = self.model_base.tokenizer
@@ -17,8 +19,8 @@ class TextEncoder(nn.Module):
                 param.requires_grad = False
 
     def forward(self, text):
-        tokenizer_encoding = self.tokenizer_base(text, return_tensors="pt", padding=True, truncation=True)
-        out = self.model_base(**tokenizer_encoding)
+        tokenizer_encoding = self.tokenizer_base(text, return_tensors="pt", padding=True, truncation=True).to(self.device)
+        out = self.model_base(tokenizer_encoding).to(self.device)
         out = self.relu_1(self.fc_1(out))
         out = self.fc_2(out)
         return out
