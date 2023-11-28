10ab9a5625e0e0f7c840486e841f9273e5c9e088
Erick Siavichay <ericksiavichay40@gmail.com>
2023-11-28 14:26:30 -0800

    fixed tokenizer

--- models/text.py
+++ models/text.py
@@ -7,7 +7,8 @@ class TextEncoder(nn.Module):
         super().__init__()
 
         self.model_base = SentenceTransformer("BAAI/bge-large-en-v1.5")
-        self.fc_1 = nn.Linear(self.model_base.config.hidden_size, 1024)
+        self.tokenizer_base = self.model_base.tokenizer
+        self.fc_1 = nn.Linear(self.model_base.get_sentence_embedding_dimension(), 1024)
         self.relu_1 = nn.ReLU()
         self.fc_2 = nn.Linear(1024, 1024)
 
@@ -16,7 +17,8 @@ class TextEncoder(nn.Module):
                 param.requires_grad = False
 
     def forward(self, text):
-        out = self.model_base(text)
+        tokenizer_encoding = self.tokenizer_base(text, return_tensors="pt", padding=True, truncation=True)
+        out = self.model_base(**tokenizer_encoding)
         out = self.relu_1(self.fc_1(out))
         out = self.fc_2(out)
         return out
