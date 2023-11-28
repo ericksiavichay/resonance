ab3be19dd1fca833f9236f21723ab59474110610
Erick Siavichay <ericksiavichay40@gmail.com>
2023-11-28 14:01:48 -0800

    fixed frozen layer code placement

--- models/text.py
+++ models/text.py
@@ -6,14 +6,14 @@ class TextEncoder(nn.Module):
     def __init__(self, freeze_base=True):
         super().__init__()
 
-        if freeze_base:
-            for param in self.model_base.parameters():
-                param.requires_grad = False
-
         self.model_base = SentenceTransformer("BAAI/bge-large-en-v1.5")
         self.fc_1 = nn.Linear(self.model_base.config.hidden_size, 1024)
         self.relu_1 = nn.ReLU()
         self.fc_2 = nn.Linear(1024, 1024)
+
+        if freeze_base:
+            for param in self.model_base.parameters():
+                param.requires_grad = False
 
     def forward(self, text):
         out = self.model_base(text)
