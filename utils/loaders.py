185fa3e122cbd3e33711cf6ef62f05bd7844a1df
Erick Siavichay <ericksiavichay40@gmail.com>
2023-12-12 16:15:13 -0800

    added musiccaps data loader

--- utils/loaders.py
+++ utils/loaders.py
@@ -4,6 +4,7 @@ import torch
 import torchaudio
 from torch.utils.data import Dataset
 import numpy as np
+from ast import literal_eval
 
 # Enable multi-threading
 # pd.options.mode.chained_assignment = "pyarrow"
@@ -44,3 +45,31 @@ class ESC50Loader(Dataset):
         text_label = self.meta_data["category"][idx]
 
         return waveform, sample_rate, text_label
+
+
+class MusicCapsLoader(Dataset):
+    def __init__(self, path):
+        """
+        Args:
+            path (string): Path to the folder containing the MusicCaps dataset
+        """
+
+        self.path = path
+        self.meta_data = pd.read_csv("utils/musiccaps-public.csv")
+
+    def __len__(self):
+        return len(self.meta_data)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        audio_name = os.path.join(self.path, f"{idx}.wav")
+        waveform, sample_rate = torchaudio.load(audio_name)
+        waveform = waveform.mean(dim=0, keepdim=True)  # takes care of stereo sound
+        waveform = torch.squeeze(waveform, dim=0)
+        aspect_list = literal_eval(self.meta_data["aspect_list"][idx])
+        aspect_str = ", ".join(aspect_list)
+        caption = self.meta_data["caption"][idx]
+
+        return waveform, sample_rate, aspect_str, caption
